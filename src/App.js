5483751152ea1fee46022ec948f6a466d9fac90e
Ingenio Contenido Digital <luis.quinones@ingeniocontenido.co>
2022-08-22 17:57:44 -0500

    -Registras Puntaje Varias Partidas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid';
 import React, {useState, useEffect, useRef} from 'react';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
   const [turno, setTurno] = useState('X');
   const [reset, setReset] = useState(false);
   const [iswin, setIswin] = useState(null);
+  const [games, setGames] = useState({X:0,O:0,tie:0})
 
   const handleClick = (event, key)=>{
     if((data[key]==='' || data[key]==='\u00A0') && iswin===null){
@@ -36,25 +38,53 @@ const App = () => {
     ]
     
     for(let k of win){
-      data[k[0]]==="X" && data[k[1]]==="X" && data[k[2]]==="X" ? setIswin('X') : 
-      data[k[0]]==="O" && data[k[1]]==="O" && data[k[2]]==="O" ? setIswin('O') : console.log('No hay ganador')
+      if(data[k[0]]==="X" && data[k[1]]==="X" && data[k[2]]==="X"){
+        setIswin('X')
+        let newgames = {...games}
+        newgames.X+=1;
+        setGames(games => ({
+          ...games,
+          ...newgames
+        }))
+      }
+      
+      if (data[k[0]]==="O" && data[k[1]]==="O" && data[k[2]]==="O"){
+        setIswin('O')
+        let newgames = {...games}
+        newgames.O+=1;
+        setGames(games => ({
+          ...games,
+          ...newgames
+        }))
+      }
+       
     }
+    
+    if(!data.includes('') && iswin===null){ 
+      setIswin('tie'); 
+      let newgames = {...games}
+      newgames.tie+=1;
+      setGames(games => ({
+        ...games,
+        ...newgames
+      }))}
     
   }
 
-  const reiniciar = ()=>{
+  const reiniciar = (inicio)=>{
     for(let element of game.current.children) {
       element.children[0].innerText='\u00A0'  
     };
-    setReset(true)
+    setReset(inicio)
+    if(inicio) setGames({X:0,O:0,tie:0});
     setIswin(null)
   }
   
   useEffect(()=>{
     setData(['','','','','','','','','']);
-    setReset(false);
     setTurno('X')
-  },[reset,iswin])
+    console.log(games);
+  },[reset,iswin,games])
 
   return (
     <Box componet="span" sx={{
@@ -64,6 +94,10 @@ const App = () => {
     <Grid container spacing={0.5} justifyContent="center"
   alignItems="center">
       <Grid item xs={12}>
+      <Chip style={{backgroundColor:'black', color:'white'}} label={"X:"+games.X} size="medium"/>
+      <Chip style={{backgroundColor:'black', color:'white'}} label={"O:"+games.O} size="medium"/>
+      <Chip style={{backgroundColor:'black', color:'white'}} label={"Empate:"+games.tie} size="medium" />
+      <br/><br/>
         {(iswin === "X" || iswin === "O") ? 
           <Alert icon={false} variant="filled" severity="success">El Ganador es: <span style={{fontWeight:'bold'}}>{iswin}</span></Alert> : 
           <Alert icon={false} variant="filled" severity="warning">Siguiente Turno: <span style={{fontWeight:'bold'}}>{turno}</span></Alert>
@@ -81,9 +115,13 @@ const App = () => {
     })}
     </Grid>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={6}>
         <br />
-        <Button variant="contained" size="small" fullWidth={true} color="warning" onClick={reiniciar}>REINICIAR JUEGO</Button>
+        <Button variant="contained" size="small" fullWidth={true} color="success" onClick={()=>{reiniciar(false)}} >OTRA PARTIDA</Button>
+      </Grid>
+      <Grid item xs={6}>
+        <br />
+        <Button variant="contained" size="small" fullWidth={true} color="error" onClick={()=>{reiniciar(true)}}>REINICIAR JUEGO</Button>
       </Grid>
     </Grid>
     </Box>
